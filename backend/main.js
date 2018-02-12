757425ec9efdc43a53f4ab3b7829a81ea06ecf92
losfair <zhy20000919@hotmail.com>
2018-02-12 19:04:18 +0800

    Cleanup

--- backend/main.js
+++
@@ -1,6 +0,0 @@
-const launcher = require("./launcher.js");
-
-launcher.run_with_config_file(process.argv[2]).then(_ => {}).catch(e => {
-    console.log(e);
-    process.exit(1);
-});
--- backend/sleep.js
+++
@@ -1,1 +0,0 @@
-module.exports = ms => new Promise(cb => setTimeout(() => cb(), ms));
